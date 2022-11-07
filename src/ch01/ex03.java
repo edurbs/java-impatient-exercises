84605fb7ac444e4d2f96d874e988758596fb62b9
Eduardo Soares <edurbs@gmail.com>
2022-11-07 19:05:23 -0300

    ex03

---
+++ src/ch01/ex03.java
@@ -0,0 +1,38 @@
+package ch01;
+
+import java.io.Console;
+
+/*Using only the conditional operator, write a program that reads three
+integers and prints the largest. Repeat with Math.max. */
+public class ex03 {
+    public static void main(String[] args) {
+
+;
+        Integer int1 = 1;
+        Integer int2 = 2;
+        Integer int3 = 3;
+
+        System.out.println("Largest with conditionals: ");
+        System.out.println(showMax1(int1, int2, int3));
+
+        System.out.println("Largest with Math.max: ");
+        System.out.println(showMax2(int1, int2, int3));
+
+    }
+
+    private static Integer showMax1(Integer int1, Integer int2, Integer int3) {
+        
+        if(int1>int2 && int1>int3){
+            return int1;
+        }else if(int2>int1 && int2>int3){
+            return int2;
+        }else{
+            return int3;
+        }
+    }
+
+    private static Integer showMax2(Integer int1, Integer int2, Integer int3) {
+        return Math.max(int1, Math.max(int2, int3));        
+    }
+
+}
